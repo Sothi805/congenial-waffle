cc2b767e35c95a220cfe7ddb92360afa3464981c
Thi Thi <vethsothi002@gmail.com>
2025-03-03 15:05:10 +0700

    initial commit

--- main/renderer.js
+++
@@ -1,9 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-document.getElementById('start-server').addEventListener('click', () => {
-  ipcRenderer.send('start-server');
-});
-
-document.getElementById('stop-server').addEventListener('click', () => {
-  ipcRenderer.send('stop-server');
-});
